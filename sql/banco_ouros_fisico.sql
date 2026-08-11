abe2bae8df457f979624ff1623f6b1433c6921c9
LucasRamosDeCarvalho <lucascarvalhoramos09@gmail.com>
2026-08-11 12:06:23 -0300

    feat/ Faz correções necessárias

--- sql/banco_ouros_fisico.sql
+++ sql/banco_ouros_fisico.sql
@@ -1,17 +1,3 @@
-CREATE TABLE tips (
-    id SERIAL PRIMARY KEY,
-    tip TEXT NOT NULL,
-    id_farm SERIAL REFERENCES farms(id) NOT NULL
-);
-
-
-CREATE TABLE categories (
-    id SERIAL PRIMARY KEY,
-    category VARCHAR(50) NOT NULL CHECK(length(category) > 0),
-    id_tip SERIAL REFERENCES tips(id) NOT NULL
-);
-
-
 CREATE TABLE addresses (
     id SERIAL PRIMARY KEY,
     zip_code VARCHAR(32) NOT NULL CHECK(length(zip_code) > 0),
@@ -22,11 +8,47 @@ CREATE TABLE addresses (
 );
 
 
+CREATE TABLE enterprises (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL CHECK(length(name) > 0),
+    email VARCHAR(32) NOT NULL CHECK(length(email) > 0),
+    cnpj VARCHAR(14) UNIQUE NOT NULL CHECK(length(cnpj) = 14), 
+    telephone VARCHAR(13) NOT NULL CHECK(length(telephone) > 9),
+    id_address INTEGER REFERENCES addresses(id) NOT NULL
+);
+
+
+CREATE TABLE farms (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL CHECK(length(name) > 0),
+    area_property FLOAT NOT NULL,
+    region VARCHAR(20) NOT NULL CHECK(length(region) > 0),
+    poultry_capacity INTEGER NOT NULL,
+    place VARCHAR(40) NOT NULL CHECK(length(place) > 0),
+    id_adress INTEGER REFERENCES addresses(id) NOT NULL,
+    id_enterprise INTEGER REFERENCES enterprises(id) NOT NULL
+);
+
+
+CREATE TABLE tips (
+    id SERIAL PRIMARY KEY,
+    tip TEXT NOT NULL CHECK(length(tip) > 0),
+    id_farm INTEGER REFERENCES farms(id) NOT NULL
+);
+
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(50) NOT NULL CHECK(length(category) > 0),
+    id_tip INTEGER REFERENCES tips(id) NOT NULL
+);
+
+
 CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
     comment TEXT NOT NULL,
     pointing INTEGER NOT NULL,
-    id_tip SERIAL REFERENCES tips(id) NOT NULL
+    id_tip INTEGER REFERENCES tips(id) NOT NULL
 );
 
 
@@ -35,32 +57,20 @@ CREATE TABLE farm_owners (
     name VARCHAR(32) NOT NULL CHECK(length(name) > 0),
     password VARCHAR(32) NOT NULL CHECK(length(password) > 0),
     email VARCHAR(32) NOT NULL CHECK(length(email) > 0),
-    zip_code(32) VARCHAR NOT NULL CHECK(length(zip_code) > 0),
-    id_farm SERIAL REFERENCES farms(id) NOT NULL,
-    id_telephone VARCHAR REFERENCES telephone(id) NOT NULL
-);
-
-
-CREATE TABLE farms (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL CHECK(length(name)> 0),
-    area_property FLOAT NOT NULL,
-    region VARCHAR(20) NOT NULL CHECK(length(region)> 0),
-    poultry_capacity INTEGER NOT NULL,
-    place VARCHAR(40) NOT NULL CHECK(length(place)> 0),
-    id_adress SERIAL REFERENCES addresses(id) NOT NULL,
-    id_enterprise SERIAL REFERENCES enterprises(id) NOT NULL
+    zip_code VARCHAR(32) NOT NULL CHECK(length(zip_code) > 0),
+    telephone VARCHAR(13) NOT NULL CHECK(length(telephone) > 9),
+    id_farm INTEGER REFERENCES farms(id) NOT NULL
 );
 
 
 CREATE TABLE individual_goals (
     id SERIAL PRIMARY KEY,
     descripition VARCHAR,
-    type VARCHAR(30) NOT NULL CHECK(length(type)> 0),
-    status VARCHAR(30) NOT NULL CHECK(length(status)> 0),
+    type VARCHAR(30) NOT NULL CHECK(length(type) > 0),
+    status VARCHAR(30) NOT NULL CHECK(length(status) > 0),
     target_value FLOAT NOT NULL,
-    title VARCHAR(40) NOT NULL CHECK(length(title)> 0),
-    id_farm SERIAL REFERENCES farms(id) NOT NULL
+    title VARCHAR(40) NOT NULL CHECK(length(title) > 0),
+    id_farm INTEGER REFERENCES farms(id) NOT NULL
 );
 
 
@@ -69,7 +79,15 @@ CREATE TABLE water_registries (
     registration_date DATE NOT NULL,
     strart_hydrometer FLOAT NOT NULL,
     end_hydrometer FLOAT NOT NULL,
-    id_farm SERIAL REFERENCES farms(id) NOT NULL
+    id_farm INTEGER REFERENCES farms(id) NOT NULL
+);
+
+
+CREATE TABLE energy_registries (
+    id SERIAL PRIMARY KEY,
+    registration_date DATE NOT NULL,
+    energy_consumption FLOAT NOT NULL,
+    id_farm INTEGER REFERENCES farms(id) NOT NULL
 );
 
 
@@ -79,10 +97,10 @@ CREATE TABLE lots (
     delivered_chickens INT NOT NULL,
     date_birth DATE NOT NULL,
     delivery_date DATE NOT NULL,
-    gain DOUBLE NOT NULL,
-    id_enterprise SERIAL REFERENCES enterprises(id) NOT NULL,
-    id_energy_restries SERIAL REFERENCES energy_registries(id) NOT NULL,
-    id_water_restries SERIAL REFERENCES water_registries(id) NOT NULL
+    gain FLOAT NOT NULL,
+    id_enterprise INTEGER REFERENCES enterprises(id) NOT NULL,
+    id_energy_restries INTEGER REFERENCES energy_registries(id) NOT NULL,
+    id_water_restries INTEGER REFERENCES water_registries(id) NOT NULL
 );
 
 
@@ -91,30 +109,21 @@ CREATE TABLE company_employees (
     name VARCHAR(32) NOT NULL CHECK(length(name) > 0),
     zip_code VARCHAR NOT NULL,
     email VARCHAR(32) NOT NULL CHECK(length(email) > 0),
-    telephone VARCHAR NOT NULL,
-    id_enterprise SERIAL REFERENCES enterprises(id) NOT NULL
-);
-
-
-CREATE TABLE enterprises (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    email VARCHAR(32) NOT NULL CHECK(length(email) > 0),
-    cnpj VARCHAR UNIQUE NOT NULL,
-    id_address SERIAL REFERENCES addresses(id) NOT NULL
+    telephone VARCHAR(13) NOT NULL CHECK(length(telephone) > 9),
+    id_enterprise INTEGER REFERENCES enterprises(id) NOT NULL
 );
 
 
 CREATE TABLE adms (
     id SERIAL PRIMARY KEY,
     email VARCHAR(32) NOT NULL CHECK(length(email) > 0),
-    password VARCHAR(32) NOT NULL CHECK(length(password) > 0),
+    password VARCHAR(32) NOT NULL CHECK(length(password) > 0)
 );
 
 
 CREATE TABLE medication_plans (
-    id SERIAL PRIMARY KEY UNIQUE,
-    title VARCHAR(40) NOT NULL CHECK(length(title)> 0),
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(40) NOT NULL CHECK(length(title) > 0),
     date_start_validity DATE NOT NULL,
     date_end_validity DATE NOT NULL,
     description TEXT,
@@ -124,7 +133,7 @@ CREATE TABLE medication_plans (
 
 CREATE TABLE regions_medication_plans (
     id SERIAL PRIMARY KEY,
-    region VARCHAR(20) NOT NULL CHECK(length(region)> 0),
+    region VARCHAR(20) NOT NULL CHECK(length(region) > 0),
     id_plan INTEGER REFERENCES medication_plans(id) NOT NULL
 );
 
@@ -132,16 +141,16 @@ CREATE TABLE regions_medication_plans (
 CREATE TABLE vaccines (
     id SERIAL PRIMARY KEY,
     aplication_days INTEGER NOT NULL,
-    name VARCHAR(20) NOT NULL CHECK(length(name)> 0),
-    dose DOUBLE NOT NULL,
-    aplication_route VARCHAR(50) NOT NULL CHECK(length(aplication_route)> 0),
+    name VARCHAR(20) NOT NULL CHECK(length(name) > 0),
+    dose FLOAT NOT NULL,
+    aplication_route VARCHAR(50) NOT NULL CHECK(length(aplication_route) > 0),
     id_plan INTEGER REFERENCES medication_plans(id) NOT NULL
 );
 
 
 CREATE TABLE diseases (
     id SERIAL PRIMARY KEY,
-    disease VARCHAR(100) NOT NULL CHECK(length(disease)> 0),
+    disease VARCHAR(100) NOT NULL CHECK(length(disease) > 0),
     id_vacinne INTEGER REFERENCES vaccines(id) NOT NULL
 );
 
@@ -149,33 +158,25 @@ CREATE TABLE diseases (
 CREATE TABLE state_goals (
     id SERIAL PRIMARY KEY,
     description TEXT,
-    type VARCHAR(30) NOT NULL CHECK(length(type)> 0),
-    status VARCHAR(30) NOT NULL CHECK(length(status)> 0),
+    type VARCHAR(30) NOT NULL CHECK(length(type) > 0),
+    status VARCHAR(30) NOT NULL CHECK(length(status) > 0),
     target_value FLOAT NOT NULL,
-    title VARCHAR(40) NOT NULL CHECK(length(title)> 0),
+    title VARCHAR(40) NOT NULL CHECK(length(title) > 0),
     date_creation TIMESTAMP NOT NULL,
     date_end TIMESTAMP NOT NULL,
-    id_farm SERIAL REFERENCES farms(id) NOT NULL
+    id_farm INTEGER REFERENCES farms(id) NOT NULL
 );
 
 
 CREATE TABLE regions_goals (
     id SERIAL PRIMARY KEY,
-    region VARCHAR(20) NOT NULL CHECK(length(region)> 0),
+    region VARCHAR(20) NOT NULL CHECK(length(region) > 0),
     id_goal INTEGER REFERENCES state_goals(id) NOT NULL
 );
 
 
 CREATE TABLE status (
     id SERIAL PRIMARY KEY,
-    status VARCHAR(30) NOT NULL CHECK(length(status)> 0),
+    status VARCHAR(30) NOT NULL CHECK(length(status) > 0),
     id_estado INTEGER REFERENCES state_goals(id) NOT NULL
-);
-
-
-CREATE TABLE energy_registries (
-    id SERIAL PRIMARY KEY,
-    registration_date DATE NOT NULL,
-    energy_consumption DOUBLE NOT NULL,
-    id_farm INTEGER REFERENCES farms(id) NOT NULL
 );
